c4fc699ad10488b7bb8afa08d22289a4f464e14b
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-02-06 06:14:06 -0800

    second

--- pages/api/writeEmail.js
+++ pages/api/writeEmail.js
@@ -1,8 +1,13 @@
 import fs from 'fs'
 export default function handler(req, res) {
     let {html,id} = req.body
-    console.log(id)
-    fs.writeFile(`C:\\Users\\arex\\Desktop\\phish\\phishing\\pages\\api\\out\\${id}.html`,html,()=>{
+    let path = `C:\\Users\\arex\\Desktop\\phish\\phishing\\pages\\api\\out\\${id}.html`
+    let path2 = __dirname
+    path2 = path2.split('\\.next\\server\\')
+    path2 = path2[0] + '\\'+ path2[1] +`\\out\\${id}.html`
+    console.log(path)
+    console.log(path2)
+    fs.writeFile(`${path2}`,html,()=>{
         console.log('done')
     })
     res.status(200).json({ name: 'done' })
