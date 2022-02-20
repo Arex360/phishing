c4b7c72833818ffa578c266ca0520dc97def1a94
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-02-19 23:36:07 -0800

    updated image

--- pages/sections/MyHomePage.jsx
+++ pages/sections/MyHomePage.jsx
@@ -4,6 +4,7 @@ import TextArea from "../components/TextArea"
 import firebase from 'firebase/compat/app'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import { getAuth, signOut} from "firebase/auth";
+import frontImg from './front.png'
 let MyHomePage = ()=>{
     const app = firebase.initializeApp({
         apiKey: "AIzaSyC8PAjtH7S7GeSlsETbpasTzWGaxgogNYc",
@@ -37,7 +38,7 @@ let MyHomePage = ()=>{
         <section className="text-gray-600 body-font h-screen overflow-scroll">
             <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"/>
+                <img className="object-cover object-center rounded" alt="hero" src={"https://imgur.com/rI48K0V.png"}/>
                 </div>
                 <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Welcome to Phish Node
