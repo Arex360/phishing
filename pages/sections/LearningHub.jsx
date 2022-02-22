c8a419b6394a06730bb42f46d3ea85147667ebbb
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-02-22 11:16:04 +0500

    added responsiveness

--- pages/sections/LearningHub.jsx
+++ pages/sections/LearningHub.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ReactPlayer from 'react-player'
 
 let LearningHub = ()=>{
     let [video,SetVideo] = useState(false)
+    useEffect(()=>{
+        alert(window.innerWidth)
+    },[])
     return(
-        <section className="text-gray-600 body-font h-screen overflow-scroll py-14 flex flex-col gap-5">
+        <section className="text-gray-600 body-font h-screen overflow-scroll py-14 flex flex-col gap-5 ">
              <div className="row1 w-full bg-slate-200 shadow-md  p-5 mb-2 flex flex-col gap-2">
                 <h1 className="text-3xl font-bold">PhishNode : Security Center Learning Hub</h1>
                 <p>Learn how to safeguard organization across attack surfaces with <b>PhishCode Learning Hub</b></p>
@@ -20,7 +23,7 @@ let LearningHub = ()=>{
                 </div>
                 <div className="block2 flex-auto">
                    {!video && 
-                   <div className="grid grid-cols-3 gap-2">
+                   <div className="grid md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-2">
                         <VideoCard onClick={()=>SetVideo(true)} num={1} title="What is phishing?"/>
                         <VideoCard onClick={()=>SetVideo(true)} num={2} title="How dangerous , phishing really is?"/>
                         <VideoCard onClick={()=>SetVideo(true)} num={3} title="What is phishCode?"/>
@@ -44,10 +47,11 @@ let VideoCard = ({num,title, onClick})=>{
     return(
         <div onClick={onClick} className="videocard p-3 shadow-xl bg-white relative group cursor-pointer">
             <img src="https://imgur.com/ELiaCw7.png" className="absolute w-1/3 left-1/3 top-1/4 scale-50 opacity-0 hidden group-hover:block group-hover:opacity-70 duration-300" alt="" />
+            <h1 className="font-semibold absolute right-2">Video{num}</h1>
             <div className="row flex">
                 <img src="https://imgur.com/ELiaCw7.png" className="absolute w-5 h-5top-4" alt="" />
                 <img className="w-52" src="https://imgur.com/mNoYyda.png" alt="" />
-                <h1 className="font-semibold">Video{num}</h1>
+             
             </div>
             <div className="row font-bold">
                 {title}
