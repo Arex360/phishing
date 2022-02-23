2688a2082c8b3813a8ff4175b99e7a373746a5c6
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-02-23 12:40:39 +0500

    bolded

--- pages/sections/AddUser.jsx
+++ pages/sections/AddUser.jsx
@@ -100,24 +100,24 @@ let AddUser = ()=>{
                 <hr />  
             </div>
             <div className="row1 w-full flex justify-center mt-3 flex-col gap-3">
-                <label className="mx-auto text-2xl">Add User</label>
+                <label className="mx-auto text-2xl font-semibold">Add User</label>
                 <div className="line w-full h-1 bg-slate-600 scale-y-[0.2]"></div>
             </div>
             <div className={"body w-full flex justify-center items-center flex-col gap-3 mt-5 "}>
                 <div className="form w-11/12 flex flex-col gap-3">
                     <div className="r1 flex justify-between">
                         <div className="field1 flex w-2/3  flex-col gap-1">
-                            <label htmlFor="">First & last name</label>
+                            <label htmlFor="" className='font-semibold'>First & last name</label>
                             <input placeholder="First name & last name" type="text" className="px-2 border-solid border-2 w-11/12 border-slate-400 outline-none" />
                         </div>
                         <div className="field1 flex w-2/3 flex-col gap-1">
-                            <label htmlFor="">Email Address</label>
+                            <label className='font-semibold'>Email Address</label>
                             <input placeholder="name@email.com" type="text" className="px-2 border-solid border-2 border-slate-400 outline-none" />
                         </div>
                     </div>
                     <div className="r2">
                         <div className="field1 flex w-2/3  flex-col gap-1">
-                            <label htmlFor="">Role</label>
+                            <label className='font-semibold'>Role</label>
                             <select className="border-solid border-2 border-slate-400 outline-none w-1/3">
                                 <option value="User">User</option>
                                 <option value="Admin">Admin</option>
@@ -127,7 +127,7 @@ let AddUser = ()=>{
                     <h1 className='text-blue-600 cursor-pointer' onClick={()=>setForm(true)}>import user through csv</h1>
                     <div className="r3 flex justify-between w-full items-center gap-4">
                         <div className="left flex flex-col w-3/5 ">
-                            <label>Availible Groups</label>
+                            <label className='font-semibold'>Availible Groups</label>
                             <div className="groupbox border-solid border-2 border-slate-400 w-4/4 min-h-[40vh]">
                                     <div className="groupnav bg-gray-300 w-full flex flex-col p-3 gap-1">
                                             <div className="input flex justify-between w-full bg-white items-center px-3 border-solid border-2 border-slate-400">
@@ -153,7 +153,7 @@ let AddUser = ()=>{
                             <button className="bg-slate-500 text-white py-2 px-5 hover:bg-slate-900 duration-300">{`<`}</button>
                         </div>
                         <div className="right flex flex-col w-3/5 ">
-                        <label>Assigned Groups</label>
+                        <label className='font-semibold'>Assigned Groups</label>
                         <div className="groupbox border-solid border-2 border-slate-400 w-4/4 min-h-[40vh]">
                                     <div className="groupnav bg-gray-300 w-full flex flex-col p-3 gap-1">
                                             <div className="input flex justify-between w-full bg-white items-center px-3 border-solid border-2 border-slate-400">
